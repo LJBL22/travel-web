d91a00e39b01652bb6cec882a93a1f0253626839
LJBL22 <yourgitemail@example.com>
2023-07-29 00:56:48 +0800

    feat: videos effect done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// for responsive nav menu
 const burgerBtn = document.querySelector('.burger')
 const navigation = document.querySelector('.nav')
 burgerBtn.addEventListener('click', () => {
@@ -5,3 +6,39 @@ burgerBtn.addEventListener('click', () => {
   navigation.classList.toggle('active')
 }
 )
+
+// for video slider nav
+const btns = document.querySelectorAll('.slider-nav-btn')
+const slides = document.querySelectorAll('.video-slide')
+const contents = document.querySelectorAll('.content')
+// 自己參考 JS30 小挑戰寫的方法=> 比較慢 多了 0.4 ms
+// let currentSlide = btns[0]
+// let toggleOpen = function (manual) {
+//   if (currentSlide === this) {
+//     return
+//   } else {
+//     currentSlide.classList.remove('active')
+//   }
+//   btns[manual].classList.add('active')
+//   currentSlide = btns[manual];
+// }
+let toggleOpen = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove('active')
+  })
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
+  contents.forEach((content) => {
+    content.classList.remove('active')
+  })
+  btns[manual].classList.add('active')
+  slides[manual].classList.add('active')
+  contents[manual].classList.add('active')
+}
+
+btns.forEach((btn, i) => btn.addEventListener('click', () => {
+  // console.time('toggleOpen');
+  toggleOpen(i);
+  // console.timeEnd('toggleOpen');
+}))
